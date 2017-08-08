17e7ac78c301c25df32e3ad59fb76cf044cee9a6
nimajesomeshwar <30686601+nimajesomeshwar@users.noreply.github.com>
2017-08-08 18:08:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,12 +5,25 @@ console.log('Successfully');
 
 var img= document.getElementById('img');
 var marginLeft=0;
+var a=0;
 function moveRight(){
   marginLeft=marginLeft+5; 
   img.style.marginLeft=marginLeft+'px';
 }
+var marginRight=0;
+var b=0;
+function moveLeft(){
+  marginRight=marginRight+5; 
+  img.style.marginRight=marginRight+'px';
+}
 
 img.onclick=function (){
-    var interval=setInterval(moveRight,50);
-  
+    if(a<20){
+    var interval1=setInterval(moveRight,50);
+    a=a+1;
+    }
+    else{
+    var interval2=setInterval(moveLeft,50);
+    b=b+1;
+    }
 };
