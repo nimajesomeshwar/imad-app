3fb5d08ca392422fc67d22f5e60052495f9ba390
nimajesomeshwar <30686601+nimajesomeshwar@users.noreply.github.com>
2017-08-08 17:59:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,16 @@
 console.log('Loaded!');
 console.log('Successfully');
+
+//move the image
+
+var img= document.getElementById('img');
+var marginLeft=0;
+function moveRight(){
+  marginLeft=marginLeft+5; 
+  imf,style.marginLeft=marginLeft+'px';
+}
+
+img.onclick=function (){
+    var interval=srtInterval(moveRight,50);
+  
+};
