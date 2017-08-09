7a30bdfe74775e6ed2c187ee7f16c0300220fd8e
nimajesomeshwar <30686601+nimajesomeshwar@users.noreply.github.com>
2017-08-09 13:15:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,6 @@
 console.log('Loaded!');
 console.log('Successfully');
 
-var counter=0;
 var button=document.getElementById('counter');
 button.onclick=function(){
     //request
@@ -9,11 +8,13 @@ button.onclick=function(){
     //response
     request.onreadystatechange = function(){
         if(request.readystate===XMLHttpRequest.DONE){
-            
+            if(request.status===200){
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
         }
     };
     //rendering
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    
 };
