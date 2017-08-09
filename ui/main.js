3011fa6d2165d1912b987046f650aa4b5602bb66
nimajesomeshwar <30686601+nimajesomeshwar@users.noreply.github.com>
2017-08-09 14:27:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,12 +29,14 @@ submit.onclick=function(){
     request.onreadystatechange = function(){
         if(request.readyState===XMLHttpRequest.DONE){
             if(request.status===200){
+                  //make request to the server and send the name 
                    var names= request.responseText;
                    names= JSON.parse(names);
     var list='';
     for (var i=0;i<names.length;i++){
         list += '<li>' + names[i] + '</li>';
     }
+        //Capture list of names from server an display it
     var ul= document.getElementById('namelist');
     ul.innerHTML=list;
             }
@@ -45,7 +47,7 @@ submit.onclick=function(){
     var name=nameInput.value;
     request.open('GET','http://nimajesomeshwar.imad.hasura-app.io/submit-name?name='+name,true);
     request.send(null);
-    //make request to the server and send the name 
-    //Capture list of names from server an display it
+  
+
 
 };
