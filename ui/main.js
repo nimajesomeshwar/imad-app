b4b13707ffdc23b876176e5fcd32fbeb5c82a3b7
nimajesomeshwar <30686601+nimajesomeshwar@users.noreply.github.com>
2017-08-09 13:10:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,13 @@ console.log('Successfully');
 var counter=0;
 var button=document.getElementById('counter');
 button.onclick=function(){
+    //request
     var request=new XMLHttpRequest();
+    //response
+    request.onreadystatechange = function(){
+        
+    };
+    //rendering
     counter=counter+1;
     var span=document.getElementById('count');
     span.innerHTML=counter.toString();
