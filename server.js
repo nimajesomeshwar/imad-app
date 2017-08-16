92aa186c104f66939fdc09d35784328ca385c524
nimajesomeshwar <30686601+nimajesomeshwar@users.noreply.github.com>
2017-08-16 20:36:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,17 +81,9 @@ return htmlTemplate;
 }
 
 app.get('/:articleName', function (req, res) {
+    var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
 });
-
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
-
 
 var counter=0;
 app.get('/counter', function(req,res){
