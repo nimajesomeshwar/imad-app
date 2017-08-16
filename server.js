a9d1fbd7da266d5e03639c05acabd5aefcab2b9b
nimajesomeshwar <30686601+nimajesomeshwar@users.noreply.github.com>
2017-08-16 20:19:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,17 +17,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-});
-
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 //reducing code
 var articleOne={
@@ -71,6 +60,19 @@ var htmlTemplate=`<html>
 `;
 return htmlTemplate;
 }
+
+app.get('/article-one', function (req, res) {
+  res.send(createTemplate(articleOne));
+});
+
+app.get('/article-two', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/article-three', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
+
 
 var counter=0;
 app.get('/counter', function(req,res){
