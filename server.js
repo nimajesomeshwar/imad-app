53b8272d7099e354c6766be52dcc75556b907dce
nimajesomeshwar <30686601+nimajesomeshwar@users.noreply.github.com>
2017-08-17 18:11:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,6 +92,15 @@ var htmlTemplate=`<html>
 return htmlTemplate;
 }
 
+var names=[];
+app.get('/submit-name', function(req,res){
+    //request to server
+   // var name= req.params.name;
+   var name= req.query.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
 app.get('/:articleName', function (req, res) {
     var articleName=req.params.articleName;
   res.send(createTemplate(articles[articleName]));
@@ -103,14 +112,6 @@ app.get('/counter', function(req,res){
     res.send(counter.toString());
 });
 
-var names=[];
-app.get('/submit-name', function(req,res){
-    //request to server
-   // var name= req.params.name;
-   var name= req.query.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
