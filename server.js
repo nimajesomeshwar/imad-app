50278ccfdb82b59607300843b8ca461599d3d218
nimajesomeshwar <30686601+nimajesomeshwar@users.noreply.github.com>
2017-08-21 15:14:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var config={
 
 var app = express();
 app.use(morgan('combined'));
-app.use(bodyParcer.json());
+//app.use(bodyParcer.json());
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
