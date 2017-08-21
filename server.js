068d0ba31de23bdc5c3da1e731641cae9209fcfa
nimajesomeshwar <30686601+nimajesomeshwar@users.noreply.github.com>
2017-08-21 14:09:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,12 +50,12 @@ var htmlTemplate=`<html>
 return htmlTemplate;
 }
 
-function hash(input){
+function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return hashed.toString(hex);
 }
 app.get('/hash/:input',function(req,res){
-   var hashedString = hash(req.params.input); 
+   var hashedString = hash(req.params.input,'This-is-random-string'); 
    res.send(hashedString);
 });
 
