8fdd143eee5668f757f399787d7acc72de2e3ccf
nimajesomeshwar <30686601+nimajesomeshwar@users.noreply.github.com>
2017-08-21 14:08:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool= require('pg').Pool;
+var crypto=require('crypto');
 var config={
     user:'nimajesomeshwar',
     database: 'nimajesomeshwar',
@@ -49,7 +50,10 @@ var htmlTemplate=`<html>
 return htmlTemplate;
 }
 
-
+function hash(input){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString(hex);
+}
 app.get('/hash/:input',function(req,res){
    var hashedString = hash(req.params.input); 
    res.send(hashedString);
